44ca3ecf21fe266b9c44d4455cd79ef1ef62bca1
Oluwajoba <oluwajobabello@gmail.com>
2019-09-09 15:48:06 +0100

    format document / cleanup code / complete step 4 / complete app.js / complete MVP

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
-
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+
   const [homeScore, home] = useState(0);
   function increamentHomeTouchDownPoint() {
     home(homeScore + 7);
@@ -28,9 +28,7 @@ function App() {
         <div className="topRow">
           <div className="home">
             <h2 className="home__name">Lions</h2>
-
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
             <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
@@ -41,16 +39,16 @@ function App() {
         </div>
         <BottomRow />
       </section>
+
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-         
-          <button onClick = {increamentHomeTouchDownPoint} className="homeButtons__touchdown">Home Touchdown</button>
-          <button onClick = {increamentHomeFieldGoalPoint}className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick={increamentHomeTouchDownPoint} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={increamentHomeFieldGoalPoint} className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button onClick = {increamentAwayTouchDownPoint}className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick = {increamentAwayFieldGoalPoint}className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={increamentAwayTouchDownPoint} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={increamentAwayFieldGoalPoint} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
